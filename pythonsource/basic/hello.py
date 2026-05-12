99d05bc2ab8d0c9671deff5a90def6046532e8af
gwaksunah <hellojoyworldz@gmail.com>
2026-05-12 17:58:58 +0900

    파이썬 기초 문법

--- pythonsource/basic/hello.py
+++ pythonsource/basic/hello.py
@@ -1,1 +1,20 @@
 print("hello")
+
+# 줄단위 실행
+# 파이썬 자료형
+# 
+
+a = 123
+print(a)
+
+multiline = "Life is too short\nYou need python"
+print(multiline)
+
+multiline = '''Life is too short
+
+
+
+
+
+You need python'''
+print(multiline)
